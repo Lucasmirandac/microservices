83937332582b72dc6102807e876acf9a2a4363ad
Lucasmirandac <lucasdemirandacosta@gmail.com>
2022-09-23 15:41:58 -0300

    Fixing controller position on structure

--- calculator/src/main/java/br/com/erudio/services/PersonService.java
+++ calculator/src/main/java/br/com/erudio/services/PersonService.java
@@ -1,2 +1,46 @@
-package br.com.erudio.services;public class PersonService {
+package br.com.erudio.services;
+
+import br.com.erudio.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class PersonService {
+    private final AtomicLong counter = new AtomicLong();
+
+    public Person create(Person person){
+        return person;
+    }
+
+    public Person findById(String id){
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Lucas");
+        person.setLastName("Costa");
+        person.setAddress("Rua Serra dos aimores, 412, Serra Dourada, Vespasiano");
+        person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAll(){
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i<8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Lucas" + i);
+        person.setLastName("Costa"+ i);
+        person.setAddress("Rua Serra dos aimores, 412, Serra Dourada, Vespasiano"+ i);
+        person.setGender("Male"+ i);
+        return person;
+    }
 }
